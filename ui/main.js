2da0ab1078caa14f47aeaf97600743931880d2bd
nikitadas965 <nikitadas965@gmail.com>
2017-03-15 02:06:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,17 @@ console.log('Loaded!');
 //change the text of the main div
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+//move image
+
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft= marginLeft + 'px';
+}
+img.onclick= function () {
+    var interval= setInterval(moveRight , 50);
+};
+
+
+    
