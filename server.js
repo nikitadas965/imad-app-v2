f0663e462db92386a3b7a5d72771f8af94ca7708
nikitadas965 <nikitadas965@gmail.com>
2017-03-14 21:47:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
   
   app.get('/article-one.html' , function (req,res) {
-      res.send('article one will be requested');
+      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
       
   
 });
