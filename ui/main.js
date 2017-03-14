73836fc8860584e0205e44d1c0ed6df8e468a797
nikitadas965 <nikitadas965@gmail.com>
2017-03-15 02:15:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ function moveRight() {
     img.style.marginLeft= marginLeft + 'px';
 }
 img.onclick= function () {
-    var interval= setInterval(moveRight , 100);
+    var interval= setInterval(moveRight , 108);
 };
 
 
