da738b46c015dbea24bd5b9308740da8dc66da61
nikitadas965 <nikitadas965@gmail.com>
2017-03-20 17:17:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ request.onreadystatechange = function () {
      
      // capture the names and render them as a list
      
-     var names = ['name1' , 'name2' , 'name3'];
+     var names = ['name1' , 'name2' , 'name3' , 'name4'];
      var list = '';
      for(var i=0; i<names.length; i++)
      {
