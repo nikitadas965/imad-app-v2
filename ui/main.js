126c7e5ef37f8727b6eb0e535b86d185674ee290
nikitadas965 <nikitadas965@gmail.com>
2017-03-20 19:05:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,21 +33,52 @@ request.onreadystatechange = function () {
     // not sure yet
     
 };
-var nameInput = document.getElementById('name');
- var name = nameInput.value;
+
  // make the request
-   request.open('GET', 'http://nikitadas965.imad.hasura-app.io/submit-name?name=' + name, true);
+   request.open('GET', 'http://nikitadas965.imad.hasura-app.io/counter' , true);
    request.send(null);
      
      
  };
  //submit name
- 
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
  var submit = document.getElementById('submit_btn');
  submit.onclick = function () {
-     // make a request to the server and send the names
      
-     // capture the names and render them as a list
+     // create request object
+ var request = new XMLHttpRequest();
+ 
+//capture the response and store in a  variable 
+request.onreadystatechange = function () {
+     if(request.readyState === XMLHttpRequest.DONE)  {
+       //take some action
+        if(request.status === 200) {
+          
+            var names = request.responseText;
+            names = JSON.parse(names);
+            
+     var list = '';
+     for(var i=0; i< names.length; i++)
+     {
+         list += '<li>' + names[i] + '</li>';
+         
+     }
+     var ul = document.getElementById('namelist');
+     ul.innerHTML = list;
+          
+       }
+    }
+    
+    // not sure yet
+    
+};
+
+ 
+     
+ // make the request
+   request.open('GET', 'http://nikitadas965.imad.hasura-app.io/submit-name?name=' +name , true);
+   request.send(null);
      
     
  };
