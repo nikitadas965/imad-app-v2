11076e9e142256cfdc15948893da3be2389b72b1
nikitadas965 <nikitadas965@gmail.com>
2017-03-20 17:58:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,17 @@ app.get('/counter' , function (req, res) {
     counter= counter + 1;
     res.send(counter.toString());
 });
+var names = [];
+app.get('/submit-name/:name', function (req, res) {
+    
+    //get the name from the request
+     var name = req.query.name;
+      names.push(name);
+     //json  
+      res.send(JSON.stringify(names));
+      
+});
+
   
   app.get('/:articleNames' , function (req,res) {
       //articleName==article one
@@ -109,16 +120,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-app.get('/submit-name/:name', function (req, res) {
-    
-    //get the name from the request
-     var name = req.params.name;
-      names.push(name);
-     //json  
-      res.send(JSON.stringify(names));
-      
-});
 
 
 
