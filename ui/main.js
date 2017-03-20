997b4c150fef5ece5b2cae16702f620e6b01a3fa
nikitadas965 <nikitadas965@gmail.com>
2017-03-20 17:16:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,3 +30,22 @@ request.onreadystatechange = function () {
      
      
  };
+ //submit name
+ var nameInput = document.getElementById('name');
+ var name= nameInput.value;
+ var submit = document.getElementById('submit_btn');
+ submit.onclick = function () {
+     // make a request to the server and send the names
+     
+     // capture the names and render them as a list
+     
+     var names = ['name1' , 'name2' , 'name3'];
+     var list = '';
+     for(var i=0; i<names.length; i++)
+     {
+         list += '<li>' + names[i] + '</li>' ;
+         
+     }
+     var ul = document.getElementById('nameslist');
+     ul.innerHTML = list;
+ }
